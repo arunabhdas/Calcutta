8a515a714d7b86dac44aa0d546b9130977966244
Arunabh Das <arunabhdas@gmail.com>
2025-05-11 23:58:49 -0400

    Added Coil to CalcuttaUI build.gradle.kts

--- Calcutta/calcuttaui/build.gradle.kts
+++ Calcutta/calcuttaui/build.gradle.kts
@@ -52,6 +52,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    // Coil - Image loading
+    implementation(libs.coil.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
